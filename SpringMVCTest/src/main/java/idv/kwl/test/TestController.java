dae3f9fecbb75eab6565eb6391ea6e55835336b0
KuanWei <KuanWei@DESKTOP-8R38C1M>
2022-02-10 06:05:15 +0800

    TestController : test

--- SpringMVCTest/src/main/java/idv/kwl/test/TestController.java
+++ SpringMVCTest/src/main/java/idv/kwl/test/TestController.java
@@ -3,13 +3,17 @@ package idv.kwl.test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import idv.kwl.test.model.User;
+
 @Controller
 public class TestController {
 
 	@RequestMapping("hello")
-	public String hello() {
+	public String hello(User user) {
 
-		System.out.println(TestController.class + "hello spring mvc");
+		System.out.println(TestController.class + ":" + "hello spring mvc");
+		System.out.println(TestController.class + ":" + user.getUsername() + ":"
+				+ user.getPassword());
 		return "hellospringmvc";
 	}
 
--- SpringMVCTest/src/main/webapp/index.jsp
+++ SpringMVCTest/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8" isELIgnored="true"%>
+<%@ page import="idv.kwl.test.model.User"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +8,14 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>hello java</h1>
-	<a href="hello">test spring mvc</a>
+	<h1>hello java xxx2</h1>
+	<%
+	User u = new User();
+	u.setUsername("abc");
+	u.setPassword("123");
 
+	request.setAttribute("username1", u.getUsername());
+	%>
+	<a href="hello?user=<%=u%>">test spring mvc</a>
 </body>
 </html>
