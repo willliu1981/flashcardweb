e3a718546a3c87b1fd2f083b1b3c1240e36d9f5c
KuanWei <KuanWei@DESKTOP-8R38C1M>
2022-03-03 22:37:24 +0800

    測試 card multi controller

--- FlashCardWebV2/src/main/java/idv/kwl/handle/controller/concrete/CardMultiController.java
+++ FlashCardWebV2/src/main/java/idv/kwl/handle/controller/concrete/CardMultiController.java
@@ -3,9 +3,9 @@ package idv.kwl.handle.controller.concrete;
 import java.sql.Date;
 import java.time.LocalDate;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import idv.kwl.dao.concrete.CardDao;
@@ -15,52 +15,48 @@ import idv.kwl.tool.SpringUtil;
 @Controller
 @RequestMapping(value = "process/card")
 public class CardMultiController {
+	// private CardDao dao = (CardDao) SpringUtil.getBean("CardDao");
+	// @Autowired
+	// private CardDao dao;
+	private CardDao dao = (CardDao) new ClassPathXmlApplicationContext()
+			.getBean("CardDao");
 
 	@RequestMapping(value = "add")
 	public String add(Card card) {
-		CardDao dao = (CardDao) SpringUtil.getBean("CardDao");
-
+		//CardDao dao = (CardDao) SpringUtil.getBean("CardDao");
 		card.setCreate_date(Date.valueOf(LocalDate.now()));
-
 		dao.create(card);
 
 		return "testresult";
 	}
 
+	/*
 	@RequestMapping(value = "delete")
 	public String delete(int cid) {
-		CardDao dao = (CardDao) SpringUtil.getBean("CardDao");
-
 		dao.delete(cid);
-
+	
 		return "testresult";
 	}
-
+	
 	@RequestMapping(value = "update")
 	public String update(Card card, int cid) {
-		CardDao dao = (CardDao) SpringUtil.getBean("CardDao");
-
 		dao.update(card, cid);
-
+	
 		return "testresult";
 	}
-
+	
 	@RequestMapping(value = "query")
 	public String queryAll(Model model) {
-		CardDao dao = (CardDao) SpringUtil.getBean("CardDao");
-
 		model.addAttribute("list", dao.queryAll());
-
+	
 		return "testlist";
 	}
-
+	
 	@RequestMapping(value = "query/{cid}")
 	public String queryByID(Model model, @PathVariable("cid") Integer cid) {
-		CardDao dao = (CardDao) SpringUtil.getBean("CardDao");
-
 		model.addAttribute("model", dao.queryById(cid));
-
+	
 		return "testlist";
 	}
-
+	//*/
 }
