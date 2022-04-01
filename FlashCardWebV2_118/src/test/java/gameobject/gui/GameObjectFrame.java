d9547cce13b171c07ca43be27130009f788ead2d
Kuanwei <kya9988@yahoo.com.tw>
2022-04-02 01:03:29 +0800

    調整 body 位置

--- FlashCardWebV2_118/src/test/java/gameobject/gui/GameObjectFrame.java
+++ FlashCardWebV2_118/src/test/java/gameobject/gui/GameObjectFrame.java
@@ -46,7 +46,7 @@ public class GameObjectFrame extends JFrame {
 	 */
 	public GameObjectFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
+		setBounds(100, 100, 1000, 700);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
