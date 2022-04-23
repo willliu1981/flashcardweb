2d37ff7b44df281c66c831200e7aae98b043c345
Kuanwei <kya9988@yahoo.com.tw>
2022-04-23 23:46:54 +0800

    json test

--- FlashCardWebV2_254/src/test/java/test1/Test1.java
+++ FlashCardWebV2_254/src/test/java/test1/Test1.java
@@ -20,12 +20,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 
-		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(
-				"classpath:spring/test.xml");
-		XmlUrlFactory bean = classPathXmlApplicationContext
-				.getBean("XmlUrlFactory", XmlUrlFactory.class);
-		Map<String, List<String>> xmlUrls = bean.getXmlUrls();
-		xmlUrls.forEach((x, y) -> Debug.test(x, y));
+		logger.info("main");
 	}
 
 	@Test
@@ -75,10 +70,38 @@ public class Test1 {
 
 		Debug.test("car", car);
 	}
-	
+
 	@Test
 	public void test4() {
-	
+
+		String json = "[{\"animal_id\":\"10305240201056\",\"animal_kind\":\"貓\",\"animal_sex\":\"F\",\"shelter_name\":\"臺北市動物之家\"},"
+				+ "{\"animal_id\":\"10508011005002\",\"animal_kind\":\"狗\",\"animal_sex\":\"F\",\"shelter_name\":\"臺中市中途動物醫院\"}]";
+
+		String[] cat = new String[4];
+		String[] dog = new String[4];
+
+		json = json.replace("[", "").replace("]", "");
+		String catData = json.substring(json.indexOf("{"),
+				json.indexOf("}") + 1);
+		String tmp = catData.replace("{", "").replace("}", "");
+		cat = tmp.split(",");
+
+		//String dogData = json.replace(catData + ",", "");
+		json = json.replace(catData + ",", "");
+		
+		//dogData = dogData.replace("{", "").replace("}", "");
+		String dogData = json.substring(json.indexOf("{"),
+				json.indexOf("}") + 1);
+		tmp = dogData.replace("{", "").replace("}", "");
+		
+		//dog = dogData.split(",");
+		dog = tmp.split(",");
+
+		System.out.println(json);
+		System.out.println(catData);
+		System.out.println(dogData);
+		System.out.println(cat[3]);
+		System.out.println(dog[3]);
 	}
 
 }
