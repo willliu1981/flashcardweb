514642eb29012db3f426870075cbb0b3f9fec87c
Kuanwei <kya9988@yahoo.com.tw>
2022-06-03 01:19:30 +0800

    修復 proficiency 部分錯誤, 由 ids array 反轉回datas 需重新設計

--- FlashCardWebV2_402/src/main/java/idv/fc/quiz/playstrategy/modimpl/ProficiencyMod.java
+++ FlashCardWebV2_402/src/main/java/idv/fc/quiz/playstrategy/modimpl/ProficiencyMod.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.annotation.Resource;
 
@@ -19,35 +20,65 @@ public class ProficiencyMod implements QuizMod<HolderDataDTO> {
 	@Override
 	public List<HolderDataDTO> doOperation(List<HolderDataDTO> origDatas,
 			Integer num) {
+		test(origDatas);
 
 		List<HolderDataDTO> collect = origDatas.stream().sorted((x1, x2) -> {
 
 			double p1 = 0, p2 = 0;
 
 			if (x1.getFlashcardHolderDTO().getNumberOfQuizTimes() == 0) {
-				p1 = 0;
-			} else {
-				p1 = x1.getFlashcardHolderDTO().getPassTheQuizTimes()
-						/ (double) x1.getFlashcardHolderDTO()
-								.getNumberOfQuizTimes();
+				return -1;
+			} else if (x2.getFlashcardHolderDTO().getNumberOfQuizTimes() == 0) {
+				return -1;
 			}
 
-			if (x2.getFlashcardHolderDTO().getNumberOfQuizTimes() == 0) {
-				p2 = 0;
-			} else {
-				p2 = x2.getFlashcardHolderDTO().getPassTheQuizTimes()
-						/ (double) x2.getFlashcardHolderDTO()
-								.getNumberOfQuizTimes();
-			}
+			p1 = x1.getFlashcardHolderDTO().getPassTheQuizTimes() / (double) x1
+					.getFlashcardHolderDTO().getNumberOfQuizTimes();
+
+			p2 = x2.getFlashcardHolderDTO().getPassTheQuizTimes() / (double) x2
+					.getFlashcardHolderDTO().getNumberOfQuizTimes();
 
 			if (p1 == p2) {
 				return 0;
 			}
 
-			return p1 > p2 ? -1 : 1;
+			return p1 < p2 ? -1 : 1;
 		}).limit(num).collect(Collectors.toList());
 
 		return collect;
+	}
+
+	private void test(List<HolderDataDTO> origDatas) {
+		origDatas.stream().sorted((x1, x2) -> {
+
+			double p1 = 0, p2 = 0;
+
+			if (x1.getFlashcardHolderDTO().getNumberOfQuizTimes() == 0) {
+				return -1;
+			} else if (x2.getFlashcardHolderDTO().getNumberOfQuizTimes() == 0) {
+				return -1;
+			}
+
+			p1 = x1.getFlashcardHolderDTO().getPassTheQuizTimes() / (double) x1
+					.getFlashcardHolderDTO().getNumberOfQuizTimes();
+
+			p2 = x2.getFlashcardHolderDTO().getPassTheQuizTimes() / (double) x2
+					.getFlashcardHolderDTO().getNumberOfQuizTimes();
+
+			if (p1 == p2) {
+				return 0;
+			}
+
+			return p1 < p2 ? -1 : 1;
+		}).forEach(x -> {
+			double p;
+
+			p = x.getFlashcardHolderDTO().getPassTheQuizTimes()
+					/ (double) x.getFlashcardHolderDTO().getNumberOfQuizTimes();
+
+			Debug.test(new Object() {
+			}, "fh id=" + x.getFlashcardHolderDTO().getId(), p);
+		});
 	}
 
 }
